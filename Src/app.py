df2e8dd4810a2f0661a2e7c8c5b5969a67e8477e
I'm Groot <paveetheran.official@gmail.com>
2021-12-31 01:49:46 +0800

    testing v0.2.4

--- Src/app.py
+++ Src/app.py
@@ -27,54 +27,65 @@ def predictPage():
         valueD7 = request.form['diabetes_pedigree_function']
         valueD8 = request.form['age']
 
-        # Heart
-        valueH1 = request.form['age_heart']
-        valueH2 = request.form['sex']
-        valueH3 = request.form['chest-pain-type']
-        valueH4 = request.form['resting-blood-pressure']
-        valueH5 = request.form['serum-cholestoral']
-        valueH6 = request.form['fasting-blood-sugar']
-        valueH7 = request.form['resting-electrocardiographic']
-        valueH8 = request.form['maximum-heart-rate']
-        valueH9 = request.form['exercise-induced-angina']
-        valueH10 = request.form['st-depression']
-        valueH11 = request.form['slope']
-        valueH12 = request.form['vessels-colored']
-        valueH13 = request.form['thalassemia']
+        # # Heart
+        # valueH1 = request.form['age_heart']
+        # valueH2 = request.form['sex']
+        # valueH3 = request.form['chest-pain-type']
+        # valueH4 = request.form['resting-blood-pressure']
+        # valueH5 = request.form['serum-cholestoral']
+        # valueH6 = request.form['fasting-blood-sugar']
+        # valueH7 = request.form['resting-electrocardiographic']
+        # valueH8 = request.form['maximum-heart-rate']
+        # valueH9 = request.form['exercise-induced-angina']
+        # valueH10 = request.form['st-depression']
+        # valueH11 = request.form['slope']
+        # valueH12 = request.form['vessels-colored']
+        # valueH13 = request.form['thalassemia']
 
-        # Liver
-        valueL1 = request.form['age_liver']
-        valueL2 = request.form['gender_liver']
-        valueL3 = request.form['total-bilirubin']
-        valueL4 = request.form['direct-bilirubin']
-        valueL5 = request.form['alkaline-phosphotase']
-        valueL6 = request.form['alamine-aminotransferase']
-        valueL7 = request.form['aspartate-aminotransferase']
-        valueL8 = request.form['total-protiens']
-        valueL9 = request.form['albumin']
-        valueL10 = request.form['albumin_and_alobulin_ratio']
+        # # Liver
+        # valueL1 = request.form['age_liver']
+        # valueL2 = request.form['gender_liver']
+        # valueL3 = request.form['total-bilirubin']
+        # valueL4 = request.form['direct-bilirubin']
+        # valueL5 = request.form['alkaline-phosphotase']
+        # valueL6 = request.form['alamine-aminotransferase']
+        # valueL7 = request.form['aspartate-aminotransferase']
+        # valueL8 = request.form['total-protiens']
+        # valueL9 = request.form['albumin']
+        # valueL10 = request.form['albumin_and_alobulin_ratio']
 
         diabetes_prediction = (valueD1,valueD2,valueD3,valueD4,valueD5,valueD6,valueD7,valueD8)
-        heart_prediction = (valueH1,valueH2,valueH3,valueH4,valueH5,valueH6,valueH7,valueH8,valueH9,valueH10,valueH11,valueH12,valueH13)
-        liver_prediction = (valueL1,valueL2,valueL3,valueL4,valueL5,valueL6,valueL7,valueL8,valueL9,valueL10)
+        # heart_prediction = (valueH1,valueH2,valueH3,valueH4,valueH5,valueH6,valueH7,valueH8,valueH9,valueH10,valueH11,valueH12,valueH13)
+        # liver_prediction = (valueL1,valueL2,valueL3,valueL4,valueL5,valueL6,valueL7,valueL8,valueL9,valueL10)
 
 
         diabetes_model = pickle.load(open("../Model/diabetes.pkl",'rb'))
-        heart_model = pickle.load(open("../Model/heart_test.pkl",'rb'))
-        liver_model = pickle.load(open("../Model/heart_test.pkl",'rb'))
+        # heart_model = pickle.load(open("../Model/heart_test.pkl",'rb'))
+        # liver_model = pickle.load(open("../Model/heart_test.pkl",'rb'))
 
 
         get_diabetes = np.asarray(diabetes_prediction)
-        get_heart = np.asarray(heart_prediction)
-        get_liver = np.asarray(liver_prediction)
+        # get_heart = np.asarray(heart_prediction)
+        # get_liver = np.asarray(liver_prediction)
 
-        prediction = model.predict(values.reshape(1, -1))[0]
+        diabetes_prediction = diabetes_model.predict(get_diabetes.reshape(1, -1))[0]
+        # heart_prediction = heart_model.predict(get_heart.reshape(1, -1))[0]
+        # liver_prediction = liver_model.predict(get_liver.reshape(1, -1))[0]
 
-        print(prediction)
 
-        if (prediction == 0):
+        if (diabetes_prediction == 0):
             print('The person is not diabetic')
         else:
-            print('The person is diabetic')    
+            print('The person is diabetic')
 
-        return render_template('index.html',result = prediction)
+        # if (heart_prediction == 0):
+        #     print('The person is not heart')
+        # else:
+        #     print('The person is heart')   
+
+        # if (liver_model == 0):
+        #     print('The person is not liver')
+        # else:
+        #     print('The person is liver')   
+
+        return render_template('index.html')
